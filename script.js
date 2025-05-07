27a2061c0be338abf4e92d2326cfafa7f60e2b7a
Arun suresh <96134187+Arunsuresh100@users.noreply.github.com>
2025-05-07 21:08:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let git_svg=document.querySelector('.bi-github');
+let whatsapp_svg=document.querySelector('.bi-whatsapp');
+git_svg.addEventListener('click',()=>{
+    window.open('https://github.com/Arunsuresh100');
+});
+whatsapp_svg.addEventListener('click',()=>{
+    alert('working on it');
+});
